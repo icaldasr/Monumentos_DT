bb950a47c570bcd1f2d3a39f1825fa2f9e5e2f1e
Diego Galarza <diegogalarza97@gmail.com>
2021-06-22 00:58:04 -0500

    Added sounds to monuments

---
+++ Monumentos_Test/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundManager : MonoBehaviour
+{
+    
+    private bool muted = false;
+
+    public void OnButtonPress()
+    {
+        if (muted == false)
+        {
+            muted = true;
+            AudioListener.pause = true;
+        }
+        else
+        {
+            muted = false;
+            AudioListener.pause = false;
+        }
+    }
+
+}
